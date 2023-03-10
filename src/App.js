9da407571d5f2f96c81ff5e2c2c47380b07cf0e9
AnnaSolovykh <ms.teachy.solo@gmail.com>
2023-03-10 10:34:03 +0300

    embedded JS in JSX

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from "react";
 
+//const title = "React"
+
+
+function getTitle(title) {
+  return title;
+}
+//We envoke a function and get back a result.
 function App() {
   return (
     <div>
-      <h1>Hello world</h1>
+      <h1>Hello {getTitle("react")}</h1>
+      {/* <h1>Hello {title}</h1> */}
+      <label htmlFor="search">Search:</label>
+      <input id="search" type="text" />
     </div>
   );
 }
