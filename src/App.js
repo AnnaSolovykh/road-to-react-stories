87fc3932b35c4b542be7991b62eefc05b2282270
AnnaSolovykh <ms.teachy.solo@gmail.com>
2023-03-10 12:50:15 +0300

    lists

--- src/App.js
+++ src/App.js
@@ -1,19 +1,55 @@
 import React from "react";
 
-//const title = "React"
+const list = [
+  {
+    title: "React",
+    url: "http://www.reactjs.org/",
+    objectID: 0,
+  },
 
+  {
+    title: "Redux",
+    url: "http://wwww.redux.js.org/",
+    objectID: 1,
+  },
 
-function getTitle(title) {
-  return title;
-}
-//We envoke a function and get back a result.
+]
 function App() {
+
+  //1 
+  const listItems = list.map( item => (
+    <li key={item.objectID}>{item.title}</li>
+  )) 
+
+  //3 
+  function listItem (item) {
+    return <li key={item.objectID}>{item.title}</li>
+  }
   return (
     <div>
-      <h1>Hello {getTitle("react")}</h1>
-      {/* <h1>Hello {title}</h1> */}
-      <label htmlFor="search">Search:</label>
-      <input id="search" type="text" />
+      <h1>My Hacker Stories</h1>
+      <hr/>
+{/* {1} */}
+      <ul>
+        {listItems}
+      </ul>
+
+{/* {2} */}
+      <ul>
+        {list.map (listitem => (
+          <li key={listitem.objectID}>
+            <span>
+              <a href={listitem.url}>{listitem.title}</a>
+            </span>            
+          </li>
+        ))}
+      </ul>
+
+{/* {3} */}
+      <ul>
+        {list.map(listItem)}
+      </ul>
+
     </div>
   );
 }
