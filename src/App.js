c4734b82188acf7765de11dd560a3c30c55cb603
AnnaSolovykh <ms.teachy.solo@gmail.com>
2023-04-21 12:35:06 +0300

    inline handlers

--- src/App.js
+++ src/App.js
@@ -2,23 +2,21 @@ import React, { useEffect, useState } from "react";
 import List from "./List";
 import InputWithLabel from "./InputWithLabel";
 
+const initialStories = [
+  {
+    title: "React",
+    url: "http://www.reactjs.org/",
+    objectID: 0,
+  },
+
+  {
+    title: "Redux",
+    url: "http://wwww.redux.js.org/",
+    objectID: 1,
+  },
+];
 
 function App() {
-
-  const stories = [
-    {
-      title: "React",
-      url: "http://www.reactjs.org/",
-      objectID: 0,
-    },
-  
-    {
-      title: "Redux",
-      url: "http://wwww.redux.js.org/",
-      objectID: 1,
-    },
-  ];
-
   const useSemiPersistentState = (key, initialState) => {
     const [value,  setValue] = 
     useState(
@@ -37,10 +35,21 @@ function App() {
   
   const [searchTerm,  setSearchTerm] = useSemiPersistentState('search', 'React');
 
+  const [stories, setStories] = useState(initialStories);
+
+  const handleRemoveStory = item => {
+    const newStories = stories.filter(
+      story => item.objectID !== story.objectID
+    );
+    setStories(newStories);
+  };
+//handleRemoveStory function filters out all the stories based on id 
+// so any id that does not match the item that we are passing it will be filtered out.
+
   const handleSearch = (event) => {
       console.log(event.target.value);
       setSearchTerm(event.target.value);
-    }
+    };
 
   const searchStories = stories.filter( (story) => 
     story.title.toLowerCase().includes(searchTerm.toLowerCase())
@@ -57,7 +66,7 @@ function App() {
         <strong>Search:</strong>
       </InputWithLabel>
       <hr></hr>
-      <List list={searchStories} title="React Ecosystem" />
+      <List list={searchStories} onRemoveItem={handleRemoveStory} title="React Ecosystem" />
     </div>
   )
   ;
--- src/List.js
+++ src/List.js
@@ -1,28 +1,25 @@
-
-const List = (props) => {
-
-    return(
-        <div>
-            <h1>{props.title}</h1>
-            <ul>
-                { props.list.map (listitem => (
-                    <Item key={listitem.objectID} item={listitem}/>
-                )
-                )}
-            </ul>
-        </div>
-    );
-}
-
+const List = ({ list, onRemoveItem }) => 
+    list.map(item => (
+        <Item 
+            key={item.objectID}
+            item={item}
+            onRemoveItem={onRemoveItem}
+        />
+    ));
 
 export default List;
- 
-  export const Item = ({item}) => {
+
+export const Item = ({ item, onRemoveItem }) => {
     return (
+    <>
         <li key={item.objectID}>
-        <span>
-            <a href={item.url}>{item.title}</a>
-        </span>            
+            <span>
+                <a href={item.url}>{item.title}</a>
+            </span>            
         </li>
+        <br></br>
+        <button type="button" onClick={()=> onRemoveItem(item)}>remove</button>
+    </>
     )
+
 }
